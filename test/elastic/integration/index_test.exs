a3de3f733446ef5380dacd5e375a4bead26803fb
Igor Kapkov <igasgeek@me.com>
2017-01-22 20:23:32 +1100

    Add open/close indices

---
+++ test/elastic/integration/index_test.exs
@@ -0,0 +1,27 @@
+defmodule Elastic.Integration.IndexTest do
+  use Elastic.IntegrationTestCase
+  alias Elastic.{HTTP, Index}
+
+  @tag integration: true
+  test "close/1 & open/1" do
+    Answer.index(1, %{text: "Hello world!"})
+
+    cat_result = get_index_response(HTTP.get("_cat/indices?format=json"), Index.name("answer"))
+    assert %{"status" => "open"} = cat_result
+
+    Index.close("answer")
+
+    cat_result = get_index_response(HTTP.get("_cat/indices?format=json"), Index.name("answer"))
+    assert %{"status" => "close"} = cat_result
+
+    Index.open("answer")
+
+    cat_result = get_index_response(HTTP.get("_cat/indices?format=json"), Index.name("answer"))
+    assert %{"status" => "open"} = cat_result
+  end
+
+  defp get_index_response({:ok, 200, indices}, name) do
+    indices
+    |> Enum.find(&(&1["index"] == name))
+  end
+end
