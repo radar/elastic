66f6ffac6f80cc3034040f05370dd4e56fd85e20
Goncalo Miranda <goncalo19961111@gmail.com>
2020-02-16 14:59:41 +0000

    fix error with status_code and json error

--- lib/elastic/response_handler.ex
+++ lib/elastic/response_handler.ex
@@ -2,7 +2,14 @@ defmodule Elastic.ResponseHandler do
   @moduledoc false
 
   def process(%{body: body, status_code: status_code}) when status_code in 400..599 do
-    {:error, status_code, decode_body(body)}
+    case decode_body(body) do 
+      {:ok, decoded_body} -> 
+        {:error, status_code, decoded_body}
+
+      {:error, error} ->
+        json_error(error)
+  end  
+    
   end
 
   def process(%{body: body, status_code: status_code}) do
@@ -10,9 +17,8 @@ defmodule Elastic.ResponseHandler do
       {:ok, decoded_body} ->
         {:ok, status_code, decoded_body}
 
-      {:error, error} ->
-        {:error, 0,
-          %{"error" => "Could not decode response into JSON, error: #{inspect(Jason.DecodeError.message(error))}"}}
+       {:error, error} ->
+        json_error(error)
     end
   end
 
@@ -36,9 +42,12 @@ defmodule Elastic.ResponseHandler do
      %{"error" => "Could not connect to Elasticsearch: request timed out (req_timedout)"}}
   end
 
-  defp decode_body("") do
-    ""
-  end
+  defp json_error(error) do 
+    {:error, 0,
+     %{"error" => "Could not decode response into JSON, error: #{inspect(Jason.DecodeError.message(error))}"}}  
+  end  
+
+  defp decode_body(""), do: ""
 
   defp decode_body(body) do
     with {:ok, decoded_body} <- Jason.decode(body), do: {:ok, decoded_body}
