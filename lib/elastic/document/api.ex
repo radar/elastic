9d6dfb0cb58d2d7052879309ee6e6ef622f4e0f7
akeddy <alex.keddy.bcs@gmail.com>
2021-05-20 10:41:39 -0300

    Adds index to the source struct

--- lib/elastic/document/api.ex
+++ lib/elastic/document/api.ex
@@ -166,8 +166,8 @@ defmodule Elastic.Document.API do
 
       def get(id, es_index \\ @es_index) do
         case raw_get(id, es_index) do
-          {:ok, 200, %{"_source" => source, "_id" => id}} ->
-            into_struct(id, source)
+          {:ok, 200, %{"_source" => source, "_id" => id, "_index" => index}} ->
+            into_struct(id, index, source)
 
           {:error, 404, %{"found" => false}} ->
             nil
@@ -189,8 +189,8 @@ defmodule Elastic.Document.API do
         result = Query.build(es_index, query) |> Index.search()
         {:ok, 200, %{"hits" => %{"hits" => hits}}} = result
 
-        Enum.map(hits, fn %{"_source" => source, "_id" => id} ->
-          into_struct(id, source)
+        Enum.map(hits, fn %{"_source" => source, "_id" => id, "_index" => index} ->
+          into_struct(id, index, source)
         end)
       end
 
@@ -215,9 +215,14 @@ defmodule Elastic.Document.API do
         Index.exists?(@es_index)
       end
 
-      defp into_struct(id, source) do
+      defp into_struct(id, index, source) do
         item = for {key, value} <- source, into: %{}, do: {String.to_atom(key), value}
-        struct(__MODULE__, Map.put(item, :id, id))
+        struct(
+          __MODULE__, 
+          item
+          |> Map.put(:id, id)
+          |> Map.put(:index, index)
+        )
       end
     end
   end
